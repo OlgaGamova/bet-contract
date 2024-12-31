f3f8daf4de809f7b68519c5a512e2a18bda4b274
Olga Gamova <olga.gamova@urfu.me>
2024-12-31 20:01:01 +0500

    Readme update

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -9,7 +9,7 @@ import { useAccount } from "wagmi";
 
 // ABI контракта
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Адрес контракта
+const CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"; // Адрес контракта
 const RPC_URL = "http://localhost:8545"; // URL локального провайдера
 
 const Home: NextPage = () => {
